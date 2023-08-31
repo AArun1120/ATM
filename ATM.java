a1d430a685ddbe4e84f499af0eebb3063392c84f
Aaron Moradi <adaar2005@gmail.com>
2023-08-31 10:07:42 -0700

    Added new methods (openAcc method)
    Added ATM class and openAcc method which opens and account while taking an id and account balance amount parameter

---
+++ ATM.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class ATM {
+    private Map<String, Double> accounts;
+
+    public ATM() {
+        accounts = new HashMap<>();
+    }
+
+    public void openAcc(double amount, String userId) {
+
+    }
+}
---
+++ ATMmain.java
@@ -0,0 +1,5 @@
+public class ATMmain {
+    public static void main(String[] args) {
+
+    }
+}
