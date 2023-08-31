2970deb32a2c940eb40154bba26b78dd7a16e9c7
Aaron Moradi <adaar2005@gmail.com>
2023-08-31 10:11:21 -0700

    Fixes and addition of method (openAccount & closeAccount)
    Fixed openAccount and added closeAccount which is used to delete a currently active account once funds are successfully withdrawn

--- ATM.java
+++ ATM.java
@@ -7,7 +7,24 @@ public class ATM {
         accounts = new HashMap<>();
     }
 
-    public void openAcc(double amount, String userId) {
+    public void openAccount(String userId, double amount) {
+        if (!accounts.containsKey(userId)) {
+            accounts.put(userId, amount);
+        } else {
+            throw new RuntimeException("User already exists");
+        }
+    }
 
+    public void closeAccount(String userId) {
+        if (accounts.containsKey(userId)) {
+            double balance = accounts.get(userId);
+            if (balance == 0) {
+                accounts.remove(userId);
+            } else {
+                throw new RuntimeException("Please withdraw balance before closing the account");
+            }
+        } else {
+            throw new RuntimeException("User not found"...);
+        }
     }
 }
